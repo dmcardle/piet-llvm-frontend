978d3ae81398f04b481e7ee48ae9a995638e7a2e
Dan McArdle <zingermc@gmail.com>
2017-12-10 14:18:31 -0500

    Separate abstract syntax from tokens

--- src/PietLang.hs
+++ src/PietLang.hs
@@ -9,34 +9,43 @@ import Debug.Trace
 type ColorBlockId = Int
 type ColorBlockConn = Maybe ColorBlockId
 
-data ColorBlock = ColorBlock { bid :: ColorBlockId,
-                               color :: PietColor,
-                               size :: Int,
-                               members :: [(Int, Int)],
-                               dp_r_cc_l :: ColorBlockId,
-                               dp_r_cc_r :: ColorBlockId,
-                               dp_d_cc_l :: ColorBlockId,
-                               dp_d_cc_r :: ColorBlockId,
-                               dp_l_cc_l :: ColorBlockId,
-                               dp_l_cc_r :: ColorBlockId,
-                               dp_u_cc_l :: ColorBlockId,
-                               dp_u_cc_r :: ColorBlockId
-                             }
+-- The raw color block resembles tokens in a traditional programming language.
+data RawColorBlock = RawColorBlock { bid :: ColorBlockId,
+                                     color :: PietColor,
+                                     size :: Int,
+                                     members :: [(Int, Int)]
+                                   }
   deriving (Eq, Show)
 
-nullColorBlock = ColorBlock {bid=0, color=Color Black Normal, size=0,
-                               members=[],
-                               dp_r_cc_l=0,
-                               dp_r_cc_r=0,
-                               dp_d_cc_l=0,
-                               dp_d_cc_r=0,
-                               dp_l_cc_l=0,
-                               dp_l_cc_r=0,
-                               dp_u_cc_l=0,
-                               dp_u_cc_r=0
-                            }
+-- The abstract color block resembles an Abstract Syntax Tree.
+data AbsColorBlock = AbsColorBlock { raw :: RawColorBlock,
+                                     dp_r_cc_l :: ColorBlockId,
+                                     dp_r_cc_r :: ColorBlockId,
+                                     dp_d_cc_l :: ColorBlockId,
+                                     dp_d_cc_r :: ColorBlockId,
+                                     dp_l_cc_l :: ColorBlockId,
+                                     dp_l_cc_r :: ColorBlockId,
+                                     dp_u_cc_l :: ColorBlockId,
+                                     dp_u_cc_r :: ColorBlockId
+                                   }
+  deriving (Eq, Show)
 
-data PietSyntaxTree = PietSyntaxTree
+nullRawColorBlock = RawColorBlock { bid=0,
+                                    color=Color Black Normal,
+                                    size=0,
+                                    members=[]
+                                  }
+
+nullAbsColorBlock = AbsColorBlock { raw=nullRawColorBlock,
+                      dp_r_cc_l=0,
+                      dp_r_cc_r=0,
+                      dp_d_cc_l=0,
+                      dp_d_cc_r=0,
+                      dp_l_cc_l=0,
+                      dp_l_cc_r=0,
+                      dp_u_cc_l=0,
+                      dp_u_cc_r=0
+                    }
 
 nullProgram = PietProgram {codels = [], width = 0, height = 0}
 
@@ -49,9 +58,9 @@ testProgram = PietProgram {codels = [[Color Red Normal,
                            width=3,
                            height=2}
 
--- Groups 4-connected components in the 2D matrix of codels into ColorBlocks.
+-- Groups 4-connected components in the 2D matrix of codels into RawColorBlocks.
 -- Also assigns names to the blocks for ease of debugging.
-lexProg :: PietProgram -> [(Int,Int)] -> [ColorBlock]
+lexProg :: PietProgram -> [(Int,Int)] -> [RawColorBlock]
 lexProg p@(PietProgram {codels=c, width=w, height=h}) visited = buildBlocks (enumCoords w h) [] 0
   where
     buildBlocks [] _ _ = []
@@ -61,11 +70,11 @@ lexProg p@(PietProgram {codels=c, width=w, height=h}) visited = buildBlocks (enu
       -- Build the block and add its members to seen
       | otherwise = thisBlock : buildBlocks xys ((members thisBlock)++seen) (cnt+1)
       where
-        thisBlock = growBlock [(x,y)] [] nullColorBlock{bid=cnt, color=colorAt p (x,y)}
+        thisBlock = growBlock [(x,y)] [] nullRawColorBlock{bid=cnt, color=colorAt p (x,y)}
 
-    growBlock :: [(Int,Int)] -> [(Int,Int)] -> ColorBlock -> ColorBlock
+    growBlock :: [(Int,Int)] -> [(Int,Int)] -> RawColorBlock -> RawColorBlock
     growBlock [] _ block = block
-    growBlock (pt@(x,y):qu) seen block@(ColorBlock{size=s, members=ms, color=col})
+    growBlock (pt@(x,y):qu) seen block@(RawColorBlock{size=s, members=ms, color=col})
       -- Pass over this point if we've already seen it
       | elem pt seen = growBlock qu seen block
       -- If the color is right, add it to the block!
@@ -73,9 +82,11 @@ lexProg p@(PietProgram {codels=c, width=w, height=h}) visited = buildBlocks (enu
       -- Wrong color -- add it to seen and continue
       | otherwise = growBlock qu (pt:seen) block
 
+    -- Defines a list of valid neighbors
     neighbors (x,y) = filter inBounds [(x+1,y), (x,y+1), (x-1,y), (x,y-1)]
     inBounds (x,y) = (0 <= x && x < w) && (0 <= y && y < h)
 
+-- List all coordinates in the rectangle from (0,0) to (w-1, h-1)
 enumCoords w h = [(x,y) | x <- [0..w-1], y <- [0..h-1]]
 
 colorAt :: PietProgram -> (Int,Int) -> PietColor
@@ -84,7 +95,9 @@ colorAt PietProgram{codels=cs, width=w, height=h} (x,y)
   | y < 0 || y >= h = Color Black Normal
   | otherwise = (cs !! y) !! x
 
--- Parse a list of ColorBlocks into a syntax tree.
+-- Parse a list of RawColorBlocks into a syntax tree.
 --
-parse :: [ColorBlock] -> PietSyntaxTree
-parse _ = PietSyntaxTree
+parse :: [RawColorBlock] -> [AbsColorBlock]
+parse rs = map buildNeighbors rs
+  where
+    buildNeighbors r = nullAbsColorBlock
