4f8e3588fdd7252f1209b04367479ad2e1eb273a
Dan McArdle <zingermc@gmail.com>
2017-12-12 22:57:50 -0500

    Getting closer with codegen

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -16,6 +16,8 @@ import LLVM.Context
 import LLVM.Module as M
 import qualified LLVM.AST.Constant as Const
 import qualified LLVM.AST.Type as T
+import qualified LLVM.AST.Operand as Oper
+import qualified LLVM.AST.IntegerPredicate as IntPred
 
 import Control.Monad.Except
 import qualified Data.ByteString       as B
@@ -28,24 +30,47 @@ int = IntegerType 32
 
 defColorBlock :: AbsColorBlock -> Definition
 defColorBlock c = GlobalDefinition functionDefaults
-  { name = Name (BS.toShort $ B.concat ["colorBlock", BC.pack $ show $ bid $ rawBlock c])
+  { name = Name (BS.toShort $ B.concat ["colorBlock", BC.pack colorBlockId])
   , parameters =
-      ( [ Parameter int (Name "a") []
-        , Parameter int (Name "b") [] ]
+      ( [ Parameter (T.ptr T.i32) (Name "stackptr") []
+        , Parameter T.i8 (Name "dp") []
+        , Parameter T.i8 (Name "cc") []
+        ]
       , False )
   , returnType = int
-  , basicBlocks = [body]
+  , basicBlocks = createConditionals $ nextBlockLookup c
   }
   where
-    body = BasicBlock
-        (Name "entry")
-        [ Name "result" :=
-            Add False  -- no signed wrap
-                False  -- no unsigned wrap
-                (LocalReference int (Name "a"))
-                (LocalReference int (Name "b"))
-                []]
-        (Do $ Ret (Just (LocalReference int (Name "result"))) [])
+    colorBlockId = show $ bid $ rawBlock c
+
+    i8Var name = LocalReference T.i8 (Name name)
+    dpVar = i8Var "dp"
+    ccVar = i8Var "cc"
+    comboVar = i8Var "combo"
+
+    createConditionals :: [(PietDirPointer, PietCodelChooser, ColorBlockId)] -> [BasicBlock]
+    createConditionals [] = []
+    createConditionals ((dp,cc,blk):xs) = newBasicBlocks
+      where
+        newBasicBlocks = [
+          BasicBlock (Name "basic_block")
+            [
+              {-
+              Name "dp_check0" := ICmp IntPred.EQ dpVar (ConstantOperand (Const.Int 8 0)) [],
+              Name "dp_check1" := ICmp IntPred.EQ dpVar (ConstantOperand (Const.Int 8 1)) [],
+              Name "dp_check2" := ICmp IntPred.EQ dpVar (ConstantOperand (Const.Int 8 2)) [],
+              Name "dp_check3" := ICmp IntPred.EQ dpVar (ConstantOperand (Const.Int 8 3)) [],
+              Name "cc_check0" := ICmp IntPred.EQ ccVar (ConstantOperand (Const.Int 8 0)) [],
+              Name "cc_check1" := ICmp IntPred.EQ ccVar (ConstantOperand (Const.Int 8 1)) [],
+-}
+              Name "dpShift" := Shl True True dpVar (ConstantOperand (Const.Int 8 3)) [],
+              Name "combo" := Add True True (i8Var "dpShift") ccVar [],
+              Name "combo_0" := ICmp IntPred.EQ comboVar (ConstantOperand (Const.Int 8 0)) []
+            ]
+            (Do $ CondBr comboVar (Name "trueDest") (Name "falseDest") [])
+          ]
+
+
 
 createModule :: [AbsColorBlock] -> LLVM.AST.Module
 createModule blocks@(firstBlock:_) = defaultModule
