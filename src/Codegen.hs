0f9494573327c5f33c97096d628f925869c230b6
Dan McArdle <zingermc@gmail.com>
2017-12-13 19:51:41 -0500

    Drafted the IR block generator, but getting runtime error.
    piet-llvm: EncodeException "reference to undefined global: Name \"ColorBlock\\\"0\\\"\""

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -28,55 +28,95 @@ import qualified Data.ByteString.Char8 as BC
 int :: Type
 int = IntegerType 32
 
+concatAsName :: [String] -> Name
+concatAsName xs = Name (BS.toShort $ B.concat $ map BC.pack xs)
+
 defColorBlock :: AbsColorBlock -> Definition
 defColorBlock c = GlobalDefinition functionDefaults
-  { name = Name (BS.toShort $ B.concat ["colorBlock", BC.pack colorBlockId])
+  { name = makeFuncName colorBlockId
   , parameters =
-      ( [ Parameter (T.ptr T.i32) (Name "stackptr") []
+      ( [
+          Parameter (T.ptr T.i32) (Name "stackptr") []
         , Parameter T.i8 (Name "dp") []
         , Parameter T.i8 (Name "cc") []
         ]
       , False )
   , returnType = int
-  , basicBlocks = createConditionals $ nextBlockLookup c
+  , basicBlocks = setupBasicBlocks ++ (createConditionals 1 (nextBlockLookup c)) ++ exitBlocks
   }
   where
+    makeFuncName blockId = concatAsName ["ColorBlock", show blockId]
+
+    colorBlockId :: String
     colorBlockId = show $ bid $ rawBlock c
 
+    --i8Var :: String -> Operand
     i8Var name = LocalReference T.i8 (Name name)
+
     dpVar = i8Var "dp"
     ccVar = i8Var "cc"
     comboVar = i8Var "combo"
 
-    createConditionals :: [(PietDirPointer, PietCodelChooser, ColorBlockId)] -> [BasicBlock]
-    createConditionals [] = []
-    createConditionals ((dp,cc,blk):xs) = newBasicBlocks
-      where
-        newBasicBlocks = [
-          BasicBlock (Name "basic_block")
-            [
-              {-
-              Name "dp_check0" := ICmp IntPred.EQ dpVar (ConstantOperand (Const.Int 8 0)) [],
-              Name "dp_check1" := ICmp IntPred.EQ dpVar (ConstantOperand (Const.Int 8 1)) [],
-              Name "dp_check2" := ICmp IntPred.EQ dpVar (ConstantOperand (Const.Int 8 2)) [],
-              Name "dp_check3" := ICmp IntPred.EQ dpVar (ConstantOperand (Const.Int 8 3)) [],
-              Name "cc_check0" := ICmp IntPred.EQ ccVar (ConstantOperand (Const.Int 8 0)) [],
-              Name "cc_check1" := ICmp IntPred.EQ ccVar (ConstantOperand (Const.Int 8 1)) [],
--}
-              Name "dpShift" := Shl True True dpVar (ConstantOperand (Const.Int 8 3)) [],
-              Name "combo" := Add True True (i8Var "dpShift") ccVar [],
-              Name "combo_0" := ICmp IntPred.EQ comboVar (ConstantOperand (Const.Int 8 0)) []
-            ]
-            (Do $ CondBr comboVar (Name "trueDest") (Name "falseDest") [])
+    setupBasicBlocks :: [BasicBlock]
+    setupBasicBlocks = [
+      BasicBlock (concatAsName ["set_vars", colorBlockId])
+        [
+          -- Make room for the CC value in combo
+          Name "dpShift" := Shl True True dpVar (ConstantOperand (Const.Int 8 1)) [],
+          Name "combo" := Add True True (i8Var "dpShift") ccVar []
           ]
+        (Do $ Br (concatAsName ["decide", show 1]) [])
+      ]
+
+    -- Create a list of LLVM basic blocks that implement this color block's
+    -- next-block lookup table.
+    createConditionals :: Int -> [(PietDirPointer, PietCodelChooser, ColorBlockId)] -> [BasicBlock]
+    createConditionals _ [] = []
+    createConditionals n ((dp,cc,blk):xs) = newBasicBlocks ++ createConditionals (n+1) xs
+      where
+        expectedComboVal = 2*(fromEnum dp) + (fromEnum cc)
+        expectedComboVar = concatAsName $ ["combo_is_", show expectedComboVal]
+        newBasicBlocks = [
+          BasicBlock (concatAsName ["decide", show n])
+            [
+              expectedComboVar := ICmp IntPred.EQ comboVar (ConstantOperand (Const.Int 8 0)) []
+            ]
+            (Do $ CondBr (LocalReference T.i8 expectedComboVar) (concatAsName ["handle", show expectedComboVal]) (concatAsName ["decide", (show (n+1))]) [])
+          ,
+          BasicBlock (concatAsName ["handle", show expectedComboVal])
+            []
+            (Do $ Invoke {
+                callingConvention'=CC.Fast
+                ,returnAttributes'=[]
+                ,function'=(Right $ ConstantOperand $ Const.GlobalReference (T.FunctionType T.VoidType [T.i8, T.i8, T.i8] False) (makeFuncName $ show blk))
+                ,arguments'=[]
+                ,functionAttributes'=[]
+                ,returnDest=concatAsName ["exit_normal"]
+                ,exceptionDest=concatAsName ["exit_exception"]
+                ,metadata'=[]
+                })
+          ]
+
+    exitBlocks :: [BasicBlock]
+    exitBlocks = [
+          BasicBlock (concatAsName ["exit_normal"])
+            []
+            (Do $ Ret Nothing []),
+          BasicBlock (concatAsName ["exit_exception"])
+            []
+            (Do $ Ret Nothing [])
+          ]
+
 
 
 
 createModule :: [AbsColorBlock] -> LLVM.AST.Module
 createModule blocks@(firstBlock:_) = defaultModule
   { moduleName = "basic"
-  , moduleDefinitions = map defColorBlock blocks
+  , moduleDefinitions = forwardDeclarations ++ map defColorBlock blocks
   }
+  where
+    forwardDeclarations = []
 
 toLLVM :: [AbsColorBlock]-> IO ()
 toLLVM colorBlocks = withContext $ \ctx -> do
