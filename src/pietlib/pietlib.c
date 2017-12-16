c02e7aac45b9b806d0be6991c61b235608c57db1
Dan McArdle <zingermc@gmail.com>
2017-12-15 23:53:28 -0500

    Improve debug output of pietlib

--- src/pietlib/pietlib.c
+++ src/pietlib/pietlib.c
@@ -10,35 +10,36 @@ void nextBlock(int * stackPtr,
                char dp,
                char cc,
                void (*funcTable[8])(int*, char, char),
-               int combo,
                int oldHue,
                int oldLightness,
                int newHue,
                int newLightness,
                int curBlockNum) {
+
+  int combo = 2*dp + cc;
+
+  //
+  // Print the lookup table.
+  //
   printf("In block %d\n\tcombo = %d\n\toldHue = %d\n\toldLightness = %d"
          "\n\tnewHue = %d\n\tnewLightness = %d\n",
          curBlockNum, combo, oldHue, oldLightness, newHue, newLightness);
 
-  int i;
-  for (i=0; i<8; i++) {
-    printf("\t-> Pointer %d = %p\n", i, funcTable[combo]);
+  int _dp, _cc;
+  for (_dp=0; _dp<4; _dp++) {
+    for (_cc=0; _cc<2; _cc++) {
+      int _combo = _dp*2 + _cc;
+      printf("\t%sCombo %d (DP %d, CC %d) -> Pointer %p\n",
+             combo == _combo ? "(*)" : "   ",
+             _combo, _dp, _cc, funcTable[_combo]);
+    }
   }
 
+  //
+  // Jump to the next block.
+  //
   funcTable[combo](stackPtr, dp, cc);
 }
-
-/*
-void FooBlock(int * stackPtr, char dp, char cc) {
-
-  void (*funcTable[8])(int*, char, char);
-  funcTable[0] = ColorBlock1;
-  funcTable[1] = ColorBlock1;
-
-  nextBlock(stackPtr, dp, cc, funcTable, 6, 5, 4, 3, 2, 1);
-}
-*/
-
 
 int main(int argc, char *argv[]) {
   int *stackPtr = &PietStack[0];
@@ -47,4 +48,3 @@ int main(int argc, char *argv[]) {
   printf("Hello");
   return 0;
 }
-
